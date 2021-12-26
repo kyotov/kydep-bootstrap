2a6f3262b2b85c956a7987cf975a61b9ecba80af
Kamen Yotov <kamen@hudson-trading.com>
2021-12-25 21:05:55 -0500

    fix

--- kydep.cmake
+++ kydep.cmake
@@ -1,22 +1,11 @@
 include_guard(GLOBAL)
 
 include(ExternalProject)
-include(FetchContent)
 
-# fetch definitions from github
-FetchContent_Declare(definitions
-    GIT_REPOSITORY https://github.com/kyotov/kydep-definitions.git
-    GIT_TAG main
-)
-FetchContent_MakeAvailable(definitions)
-list(APPEND CMAKE_MODULE_PATH "${definitions_SOURCE_DIR}")
-
-macro(KyDep NAME)
-    include_guard(GLOBAL)
+function(KyDep NAME)
+    message(STATUS "[KYDEP] ${NAME}")
 
     set(DIR "${CMAKE_BINARY_DIR}/${NAME}")
-
-    list(APPEND KYDEPS_CONTEXT "${NAME};${ARGN};;;")
 
     ExternalProject_Add(${NAME}
         PREFIX "${DIR}"
@@ -28,8 +17,9 @@ macro(KyDep NAME)
         LOG_DIR "${DIR}/log"
 
         CMAKE_ARGS
-        -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}
-        -DCMAKE_INSTALL_PREFIX:PATH=${DIR}/i
+        -D CMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}
+        -D CMAKE_INSTALL_PREFIX:PATH=${DIR}/i
+        -D "CMAKE_MESSAGE_INDENT=${CMAKE_MESSAGE_INDENT}[${NAME}]"
 
         ${ARGN}
     )
@@ -38,7 +28,6 @@ macro(KyDep NAME)
         CMAKE_ARGS
         -DCMAKE_PREFIX_PATH:PATH=${DIR}/i
         DEPENDS ${NAME}
+        PARENT_SCOPE
     )
-
-    unset(DIR)
-endmacro()
+endfunction()
